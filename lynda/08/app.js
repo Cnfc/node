6dd6430918ca1deb61b3ff69e9f95a854a2d6ad8
cnfc <stdashkov@mail.ru>
2018-04-25 15:56:14 +0300

    add json skierTerms

--- lynda/08/app.js
+++ lynda/08/app.js
@@ -1,6 +1,23 @@
 var express = require("express");
-
+var cors = require("cors");
 var app = express();
+
+var skierTerms = [
+    {
+        term: "Rip",
+        defined: "To move at a high rate of speed"
+    },
+    {
+        term: "Huck",
+        defined: "To throw your body off of something, usually a natural feature like a cliff"
+    },
+    {
+        term: "Chowder",
+        defined: "Powder after it has been sufficiently skied"
+    }
+];
+
+
 
 app.use(function(req, res, next){
 
@@ -10,8 +27,28 @@ app.use(function(req, res, next){
 
 app.use(express.static("./public"));
 
+app.usr(cors());
+
+app.get("/dictionary-api", function(req, res){
+    res.json(skierTerms);
+});
 app.listen(3000);
 
 console.log("Express app running on port 3000");
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
