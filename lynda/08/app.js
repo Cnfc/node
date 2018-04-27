602a6792ac4c5b19e45f9bf62edc5ebb496c23c2
cnfc <stdashkov@mail.ru>
2018-04-27 16:41:23 +0300

    POST-DELETE

--- lynda/08/app.js
+++ lynda/08/app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var cors = require("cors");
+var bodyParser = require("body-parser");
 var app = express();
+
 
 var skierTerms = [
     {
@@ -17,38 +19,35 @@ var skierTerms = [
     }
 ];
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-
-app.use(function(req, res, next){
-
-    console.log(`${req.method} request for '${req.url}'`);
-    next();
+app.use(function(req, res, next) {
+	console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
+	next();
 });
+
 
 app.use(express.static("./public"));
 
-app.usr(cors());
+app.use(cors());
 
-app.get("/dictionary-api", function(req, res){
+app.get("/dictionary-api", function(req, res) {
     res.json(skierTerms);
 });
+app.post("/dictionary-api", function(req, res){
+    skierTerms.push(req.body);
+    res.json(skierTerms);
+});
+app.delete("/dictionary-api/:term", function(req, res){
+    skierTerms = skierTerms.filter(function(definition) {
+        return definition.term.toLowerCase() !== req.params.term.toLowerCase();
+    });
+    res.json(skierTerms);
+});
+
 app.listen(3000);
 
 console.log("Express app running on port 3000");
 
 module.exports = app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
